dea36da1aa46219e8cf4a73dc3da2a9a6e40898c
ChiaLing <hochialing91@gmail.com>
2020-04-07 00:35:52 +0200

    Updates

---
+++ precache-manifest.d455ff3cac7bb3497ae3553005bb8f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5663123c74aef64bc75f36a34bb980",
+    "url": "/work-shift-calendar/index.html"
+  },
+  {
+    "revision": "7838cc451f3ed40973af",
+    "url": "/work-shift-calendar/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "5091fa7b2df5b947ff08",
+    "url": "/work-shift-calendar/static/css/main.3e02bd57.chunk.css"
+  },
+  {
+    "revision": "7838cc451f3ed40973af",
+    "url": "/work-shift-calendar/static/js/2.26fd8a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/work-shift-calendar/static/js/2.26fd8a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5091fa7b2df5b947ff08",
+    "url": "/work-shift-calendar/static/js/main.bd990d9f.chunk.js"
+  },
+  {
+    "revision": "08100dbc34533a0b4ac3",
+    "url": "/work-shift-calendar/static/js/runtime-main.99150c75.js"
+  }
+]);
